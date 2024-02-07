4a080f7da2bb2287fc6fc758e16e2c6830c80210
MedNewton <med.newton.2013@gmail.com>
2024-02-07 10:06:08 -0800

    .

--- src/components/team.tsx
+++ src/components/team.tsx
@@ -2,7 +2,19 @@
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
 
+import t1 from '@assets/FabrizioGenovese.jpg'
+import t2 from '@assets/andrea.jpeg'
+import t3 from '@assets/raffa.jpg'
+import t4 from '@assets/ionut.jpeg'
+import t5 from '@assets/gianluca.jpeg'
+import t6 from '@assets/lore.jpeg'
+import t7 from '@assets/Illustrator.jpg'
+import { useState } from "react";
+
 function Team() {
+
+    const [selectedSliderIndex, setSelectedSliderIndex] = useState<number>(0)
+    const indices = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
     return (
         <section className="w-full flex flex-col items-center justify-start pt-24">
             <h5 className=" text-black text-5xl mb-4 lg:mb-10 SemiBold leading-normal text-center">
@@ -10,21 +22,27 @@ function Team() {
             </h5>
             <div className="h-fit hidden lg:flex flex-row items-stretch justify-between w-10/12 mb-12">
                 <div className="w-1/3 flex flex-col flex-grow items-center justify-center">
-                    <div className="w-8/12 aspect-square bg-white buttonShadow mb-2"></div>
+                    <div className="w-8/12 aspect-square bg-cover bg-top bg-no-repeat buttonShadow mb-2" style={{
+                        backgroundImage: `url('${t1.src}')`
+                    }}></div>
                     <div className="w-8/12 h-1/3 flex flex-col items-center justify-center py-2 px-4 bg-white buttonShadow">
                         <h5 className=" text-black w-full SemiBold text-2xl text-center mb-2">Fabrizio R. Genovese</h5>
                         <h5 className=" text-[#606060] SemiBold text-center text-3xl">Strategy Lead</h5>
                     </div>
                 </div>
                 <div className="w-1/3 flex flex-col flex-grow items-center justify-center">
-                    <div className="w-8/12 aspect-square bg-white buttonShadow mb-2"></div>
+                    <div className="w-8/12 aspect-square bg-cover bg-center bg-no-repeat buttonShadow mb-2" style={{
+                        backgroundImage: `url('${t2.src}')`
+                    }}></div>
                     <div className="w-8/12 h-1/3 flex flex-col items-center justify-center py-2 px-4 bg-white buttonShadow">
                         <h5 className=" text-black w-full SemiBold text-2xl text-center mb-2">Andrea Paesano</h5>
                         <h5 className=" text-[#606060] SemiBold text-center text-3xl">Event Lead</h5>
                     </div>
                 </div>
                 <div className="w-1/3 flex flex-grow flex-col items-center justify-center">
-                    <div className="w-8/12 aspect-square bg-white buttonShadow mb-2"></div>
+                    <div className="w-8/12 aspect-square bg-cover bg-center bg-no-repeat buttonShadow mb-2" style={{
+                        backgroundImage: `url('${t3.src}')`
+                    }}></div>
                     <div className="w-8/12 h-1/3 flex flex-col items-center justify-center py-2 px-4 bg-white buttonShadow">
                         <h5 className=" text-black SemiBold text-2xl text-center mb-2">Raffaele Orifece</h5>
                         <h5 className=" text-[#606060] SemiBold text-center text-3xl">Community Lead</h5>
@@ -34,21 +52,27 @@ function Team() {
             </div>
             <div className="h-fit hidden lg:flex flex-row items-stretch justify-between w-10/12 mb-12">
                 <div className="w-1/3 flex flex-col flex-grow items-center justify-center">
-                    <div className="w-8/12 aspect-square bg-white buttonShadow mb-2"></div>
+                    <div className="w-8/12 aspect-square bg-cover bg-center bg-no-repeat buttonShadow mb-2" style={{
+                        backgroundImage: `url('${t4.src}')`
+                    }}></div>
                     <div className="w-8/12 h-1/3 flex flex-col items-center justify-center py-2 px-4 bg-white buttonShadow">
                         <h5 className=" text-black w-full SemiBold text-2xl text-center mb-2">Ionut Gaucan</h5>
                         <h5 className=" text-[#606060] SemiBold text-center text-3xl">Business Dev.</h5>
                     </div>
                 </div>
                 <div className="w-1/3 flex flex-col flex-grow items-center justify-center">
-                    <div className="w-8/12 aspect-square bg-white buttonShadow mb-2"></div>
+                    <div className="w-8/12 aspect-square bg-cover bg-center bg-no-repeat buttonShadow mb-2" style={{
+                        backgroundImage: `url('${t5.src}')`
+                    }}></div>
                     <div className="w-8/12 h-1/3 flex flex-col items-center justify-center py-2 px-4 bg-white buttonShadow">
                         <h5 className=" text-black w-full SemiBold text-2xl text-center mb-2">Gianluca Di Bella</h5>
                         <h5 className=" text-[#606060] SemiBold text-center text-3xl">Strategic Partner</h5>
                     </div>
                 </div>
                 <div className="w-1/3 flex flex-grow flex-col items-center justify-center">
-                    <div className="w-8/12 aspect-square bg-white buttonShadow mb-2"></div>
+                    <div className="w-8/12 aspect-square bg-cover bg-center bg-no-repeat buttonShadow mb-2" style={{
+                        backgroundImage: `url('${t6.src}')`
+                    }}></div>
                     <div className="w-8/12 h-1/3 flex flex-col items-center justify-center py-2 px-4 bg-white buttonShadow">
                         <h5 className=" text-black SemiBold text-2xl text-center mb-2">Lorenzo Cappluni</h5>
                         <h5 className=" text-[#606060] SemiBold text-center text-3xl">Strategic Partner</h5>
@@ -61,7 +85,9 @@ function Team() {
 
                 </div>
                 <div className="w-1/3 flex flex-col flex-grow items-center justify-center">
-                    <div className="w-8/12 aspect-square bg-white buttonShadow mb-2"></div>
+                    <div className="w-8/12 aspect-square bg-cotain bg-center bg-no-repeat buttonShadow mb-2" style={{
+                        backgroundImage: `url('${t7.src}')`
+                    }}></div>
                     <div className="w-8/12 h-1/3 flex flex-col items-center justify-center py-2 px-4 bg-white buttonShadow">
 
                         <h5 className=" text-black SemiBold text-center text-3xl">The Illustrator</h5>
@@ -74,10 +100,14 @@ function Team() {
             </div>
             <Carousel className="w-full h-fit block lg:hidden" showIndicators={false} swipeable={true}
                 preventMovementUntilSwipeScrollTolerance={true}
-                swipeScrollTolerance={50} interval={3000} autoPlay infiniteLoop showArrows={false} showStatus={false} showThumbs={false}>
+                swipeScrollTolerance={50} interval={3000} autoPlay infiniteLoop showArrows={false} showStatus={false} showThumbs={false}
+                onChange={(index: number)=>{setSelectedSliderIndex(index)}} selectedItem={selectedSliderIndex}
+                >
                 <div className="w-full h-fit flex flex-row items-center justify-center">
                     <div className="w-full flex flex-col flex-grow items-center justify-center">
-                        <div className="w-8/12 aspect-square bg-white buttonShadow mb-2"></div>
+                        <div className="w-8/12 aspect-square bg-cover bg-top bg-no-repeat buttonShadow mb-2" style={{
+                        backgroundImage: `url('${t1.src}')`
+                    }}></div>
                         <div className="w-8/12 h-1/3 flex flex-col items-center justify-center py-2 px-4 bg-white buttonShadow">
                             <h5 className=" text-black w-full SemiBold text-2xl text-center mb-2">Fabrizio R. Genovese</h5>
                             <h5 className=" text-[#606060] SemiBold text-center text-3xl">Strategy Lead</h5>
@@ -86,7 +116,9 @@ function Team() {
                 </div>
                 <div className="w-full h-fit flex flex-row items-center justify-center">
                     <div className="w-full flex flex-col flex-grow items-center justify-center">
-                        <div className="w-8/12 aspect-square bg-white buttonShadow mb-2"></div>
+                        <div className="w-8/12 aspect-square bg-cover bg-center bg-no-repeat buttonShadow mb-2" style={{
+                        backgroundImage: `url('${t2.src}')`
+                    }}></div>
                         <div className="w-8/12 h-1/3 flex flex-col items-center justify-center py-2 px-4 bg-white buttonShadow">
                             <h5 className=" text-black w-full SemiBold text-2xl text-center mb-2">Andrea Paesano</h5>
                             <h5 className=" text-[#606060] SemiBold text-center text-3xl">Event Lead</h5>
@@ -95,7 +127,9 @@ function Team() {
                 </div>
                 <div className="w-full h-fit flex flex-row items-center justify-center">
                     <div className="w-full flex flex-col flex-grow items-center justify-center">
-                        <div className="w-8/12 aspect-square bg-white buttonShadow mb-2"></div>
+                        <div className="w-8/12 aspect-square bg-cover bg-center bg-no-repeat buttonShadow mb-2" style={{
+                        backgroundImage: `url('${t3.src}')`
+                    }}></div>
                         <div className="w-8/12 h-1/3 flex flex-col items-center justify-center py-2 px-4 bg-white buttonShadow">
                             <h5 className=" text-black SemiBold text-2xl text-center mb-2">Raffaele Orifece</h5>
                             <h5 className=" text-[#606060] SemiBold text-center text-3xl">Community Lead</h5>
@@ -104,7 +138,9 @@ function Team() {
                 </div>
                 <div className="w-full h-fit flex flex-row items-center justify-center">
                     <div className="w-full flex flex-col flex-grow items-center justify-center">
-                        <div className="w-8/12 aspect-square bg-white buttonShadow mb-2"></div>
+                        <div className="w-8/12 aspect-square bg-cover bg-center bg-no-repeat buttonShadow mb-2" style={{
+                        backgroundImage: `url('${t4.src}')`
+                    }}></div>
                         <div className="w-8/12 h-1/3 flex flex-col items-center justify-center py-2 px-4 bg-white buttonShadow">
                             <h5 className=" text-black w-full SemiBold text-2xl text-center mb-2">Ionut Gaucan</h5>
                             <h5 className=" text-[#606060] SemiBold text-center text-3xl">Business Dev.</h5>
@@ -113,7 +149,9 @@ function Team() {
                 </div>
                 <div className="w-full h-fit flex flex-row items-center justify-center">
                     <div className="w-full flex flex-col flex-grow items-center justify-center">
-                        <div className="w-8/12 aspect-square bg-white buttonShadow mb-2"></div>
+                        <div className="w-8/12 aspect-square bg-cover bg-center bg-no-repeat buttonShadow mb-2" style={{
+                        backgroundImage: `url('${t5.src}')`
+                    }}></div>
                         <div className="w-8/12 h-1/3 flex flex-col items-center justify-center py-2 px-4 bg-white buttonShadow">
                             <h5 className=" text-black w-full SemiBold text-2xl text-center mb-2">Gianluca Di Bella</h5>
                             <h5 className=" text-[#606060] SemiBold text-center text-3xl">Strategic Partner</h5>
@@ -122,7 +160,9 @@ function Team() {
                 </div>
                 <div className="w-full h-fit flex flex-row items-center justify-center">
                     <div className="w-full flex flex-col flex-grow items-center justify-center">
-                        <div className="w-8/12 aspect-square bg-white buttonShadow mb-2"></div>
+                        <div className="w-8/12 aspect-square bg-cover bg-center bg-no-repeat buttonShadow mb-2" style={{
+                        backgroundImage: `url('${t6.src}')`
+                    }}></div>
                         <div className="w-8/12 h-1/3 flex flex-col items-center justify-center py-2 px-4 bg-white buttonShadow">
                             <h5 className=" text-black SemiBold text-2xl text-center mb-2">Lorenzo Cappluni</h5>
                             <h5 className=" text-[#606060] SemiBold text-center text-3xl">Strategic Partner</h5>
@@ -131,7 +171,9 @@ function Team() {
                 </div>
                 <div className="w-full h-fit flex flex-row items-center justify-center">
                     <div className="w-full flex flex-col flex-grow items-center justify-center">
-                        <div className="w-8/12 aspect-square bg-white buttonShadow mb-2"></div>
+                        <div className="w-8/12 aspect-square bg-cover bg-center bg-no-repeat buttonShadow mb-2" style={{
+                        backgroundImage: `url('${t7.src}')`
+                    }}></div>
                         <div className="w-8/12 h-1/3 flex flex-col items-center justify-center py-2 px-4 bg-white buttonShadow">
 
                             <h5 className=" text-black SemiBold text-center text-3xl">The Illustrator</h5>
@@ -141,6 +183,14 @@ function Team() {
 
 
             </Carousel>
+            <div className="w-fit h-fit flex lg:hidden flex-row items-center justify-center gap-2 my-6">
+                {
+                    indices.map((index: number) => (
+                        <div key={index} onClick={()=>{setSelectedSliderIndex(index)}} className={`${index == selectedSliderIndex ? " bg-gray-200 " : " bg-white"} cursor-pointer buttonShadow h-5 aspect-square`}></div>
+                    ))
+                }
+            </div>
+
         </section>
     )
 }
--- src/components/verticals.tsx
+++ src/components/verticals.tsx
@@ -10,11 +10,15 @@ import munaciello3 from "@assets/MUNACIELLO LAW.png"
 import munaciello4 from "@assets/MUNACIELLO DEFI TTOLING.png"
 import munaciello5 from "@assets/MUNACIELLO GAMING.png"
 import munaciello6 from "@assets/MUNACIELLO PUBLIC.png"
+import { useState } from "react";
 
 
 
 
 function Verticals() {
+
+    const [selectedSliderIndex, setSelectedSliderIndex] = useState<number>(0)
+    const indices = [0, 1, 2, 3, 4, 5]
     return (
         <section className="w-full flex flex-col items-center justify-start pt-12">
             <h5 className=" text-black text-5xl mb-10 SemiBold leading-normal text-center">
@@ -62,7 +66,7 @@ function Verticals() {
                 </div>
 
             </div>
-            <Carousel className="w-full h-fit block lg:hidden" showIndicators={false} swipeable={true}
+            <Carousel className="w-full h-fit block lg:hidden" onChange={(index: number)=>{setSelectedSliderIndex(index)}} selectedItem={selectedSliderIndex} showIndicators={false} swipeable={true}
                 preventMovementUntilSwipeScrollTolerance={true}
                 swipeScrollTolerance={50} interval={3000} autoPlay infiniteLoop showArrows={false} showStatus={false} showThumbs={false}>
                 <div className="w-full h-fit flex flex-row items-center justify-center">
@@ -115,6 +119,14 @@ function Verticals() {
                 </div>
 
             </Carousel>
+            <div className="w-fit h-fit flex lg:hidden flex-row items-center justify-center gap-2 my-6">
+                {
+                    indices.map((index: number) => (
+                        <div key={index} onClick={()=>{setSelectedSliderIndex(index)}} className={`${index == selectedSliderIndex ? " bg-gray-200 " : " bg-white"} cursor-pointer buttonShadow h-5 aspect-square`}></div>
+                    ))
+                }
+            </div>
+
         </section>
     )
 }
