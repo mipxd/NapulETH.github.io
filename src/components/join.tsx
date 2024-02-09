40eae8aa7fe5bf605cca9e1ac3e745b299a1f5e0
MedNewton <med.newton.2013@gmail.com>
2024-02-09 11:07:17 -0800

    .

--- src/components/join.tsx
+++ src/components/join.tsx
@@ -21,7 +21,7 @@ function JoinSection() {
                 {t('join1')}
             </p>
             <Link href={'https://docs.google.com/forms/d/e/1FAIpQLSe1U6zRTRp671KrtyW1U1fBvbrVal_irFUjTy5Oum_K7mVryg/viewform'} target="_blank" className=" relative z-50 p-0 w-fit h-fit">
-                <div className="w-fit h-fit flex flex-row items-center justify-center gap-2 py-4 px-12 bg-[#eddb55] buttonShadow my-10">
+                <div className="w-fit h-fit flex flex-row items-center justify-center gap-2 py-4 px-12 bg-[#eddb55] border-2 border-[#eddb55] my-10">
 
 
                     <h5 className=" text-black Medium text-2xl">
@@ -33,7 +33,7 @@ function JoinSection() {
                 {t('join2')}
             </p>
             <Link href={'https://docs.google.com/forms/d/e/1FAIpQLScxTrj8mKZHx_X8y7ChZoFM4TT8-rG74K2CsHz09_eXiTaqYQ/viewform'} target="_blank" className=" relative z-50 p-0 w-fit h-fit">
-                <div className="w-fit h-fit flex flex-row items-center justify-center gap-2 py-4 px-12 bg-[#E38B84] buttonShadow my-10">
+                <div className="w-fit h-fit flex flex-row items-center justify-center gap-2 py-4 px-12 bg-[#E38B84] border-2 border-[#eddb55] my-10">
 
 
                     <h5 className=" text-black Medium text-2xl">
@@ -45,7 +45,7 @@ function JoinSection() {
                 {t('join3')}
             </p>
             <Link href={'https://docs.google.com/forms/d/e/1FAIpQLScVN8T-k_r-qMjI5Z4KFgzb1ShksHdhFMBcsWRTA1qNcrVKWw/viewform'} target="_blank" className=" relative z-50 p-0 w-fit h-fit">
-                <div className="w-fit h-fit flex flex-row items-center justify-center gap-2 py-4 px-12 bg-[#9ECC51] buttonShadow my-10">
+                <div className="w-fit h-fit flex flex-row items-center justify-center gap-2 py-4 px-12 bg-[#9ECC51] border-2 border-[#eddb55] my-10">
 
 
                     <h5 className=" text-black Medium text-2xl">
@@ -57,7 +57,7 @@ function JoinSection() {
                 {t('join4')}
             </p>
             <Link href={'https://docs.google.com/forms/d/e/1FAIpQLSf5t-L7kW2xCzN-kggLruouCEi_hEKQFkJHjiN781r5lZyxaA/viewform'} target="_blank" className=" relative z-50 p-0 w-fit h-fit">
-                <div className="w-fit h-fit flex flex-row items-center justify-center gap-2 py-4 px-12 bg-[#CA75AA] buttonShadow my-10">
+                <div className="w-fit h-fit flex flex-row items-center justify-center gap-2 py-4 px-12 bg-[#CA75AA] border-2 border-[#eddb55] my-10">
 
 
                     <h5 className=" text-black Medium text-2xl">
