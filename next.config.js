e22f54688c8de128fe07efd60182e842ce34e4d3
MedNewton <med.newton.2013@gmail.com>
2024-02-11 12:07:41 -0800

    .

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
 const config = {
-      basePath: "/NapulEth/",
+
       output: "export",
       images: {
         unoptimized: true,
