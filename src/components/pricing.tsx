ff9eee979f229ac08eb5346480d9c078e6703718
MedNewton <med.newton.2013@gmail.com>
2024-02-22 04:30:45 -0800

    .

--- src/components/pricing.tsx
+++ src/components/pricing.tsx
@@ -16,12 +16,16 @@ function Pricing() {
     const { t, i18n } = useTranslation();
 
     return (
+        <>
+        <h5 className=" text-black w-full text-4xl mt-8 SemiBold leading-normal text-center">
+                Tickets Rounds
+            </h5>
         <section className="w-full flex flex-col lg:flex-row items-center lg:items-stretch justify-center gap-8 lg:px-20 mt-20 lg:mt-32">
             <div className="w-11/12 lg:w-1/2 flex-grow flex flex-col items-center justify-start">
                 <div className='w-7/12 h-fit py-4 bg-white border-4 border-[#eddb55]  relative z-40'>
                     <h5 className='Bold text-Black text-xl w-full text-center'>Pre-Booking</h5>
                 </div>
-                <div className='w-full h-fit lg:h-full bg-red-800 border-4 border-black relative z-10 -mt-9 flex flex-col items-center justify-start pt-4 pb-2 lg:pt-2 lg:pb-4 px-4'>
+                <div className='w-full h-fit lg:h-full bg-[#E38B84] border-4 border-black relative z-10 -mt-9 flex flex-col items-center justify-start pt-4 pb-2 lg:pt-2 lg:pb-4 px-4'>
                     <Image src={soldOut} alt="munaciello sold out" className='w-7/12 aspect-square mt-6 lg:mt-0 brightness-0'></Image>
                     <p className='hidden lg:block text-lg Medium text-black w-10/12 text-center'>
                         {t('earlyPara')}
@@ -82,6 +86,8 @@ function Pricing() {
             </div>
             
         </section>
+        </>
+        
     )
 }
 
