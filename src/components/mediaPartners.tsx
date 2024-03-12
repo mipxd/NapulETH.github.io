c8e3bd59a3206d264d94ef15b5e8ceee29e150c9
MedNewton <med.newton.2013@gmail.com>
2024-03-12 08:42:18 -0700

    .

--- src/components/mediaPartners.tsx
+++ src/components/mediaPartners.tsx
@@ -188,7 +188,7 @@ function MediaPartners() {
                             <Image className=" w-auto h-20 scale-150" alt="degate" src={podcast1}></Image>
                         </div>
                     </Link>
-                    <Link href={'https://bitcashback.net/'} target="_blank" className="p-0 w-full h-fit">
+                    <Link href={'https://twitter.com/ilcryptopub_'} target="_blank" className="p-0 w-full h-fit">
                         <div className="w-full lg:w-[20vw] mx-4 lg:mx-8 h-full flex flex-row items-center justify-center py-10 px-16 bg-white border-4 border-[#eddb55]">
 
                             <Image className=" w-auto h-20 scale-125" alt="degate" src={ch}></Image>
@@ -464,7 +464,7 @@ function MediaPartners() {
                 <div className="w-full h-fit flex flex-row items-center justify-center">
                     <div className="w-10/12 flex flex-col items-center justify-center">
                         <div className="w-full flex-grow flex flex-col items-center justify-center" >
-                            <Link href={'https://bitcashback.net/'} target="_blank" className="w-full h-full">
+                            <Link href={'https://twitter.com/ilcryptopub_'} target="_blank" className="w-full h-full">
                                 <div className="w-full h-full flex flex-row items-center justify-center py-10 px-16  bg-white border-4 border-[#eddb55]">
                                 <div className="w-28 h-20 scale-[1.5] bg-contain bg-center bg-no-repeat" style={{
                                         backgroundImage: `url('${ch.src}')`
