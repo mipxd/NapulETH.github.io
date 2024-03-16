4270db027a49ecd450aab76f7dac7f7878b1e984
MedNewton <med.newton.2013@gmail.com>
2024-03-16 14:39:14 -0700

    .

--- src/components/sponsors.tsx
+++ src/components/sponsors.tsx
@@ -16,6 +16,7 @@ import { useTranslation } from "react-i18next";
 import yh from '@assets/mark+word-colorful.svg'
 import socialSquare from '@assets/socialSquare.svg'
 import money from "@assets/money.svg"
+import vault from "@assets/CriptovalutaIt marchio registrato.jpg"
 
 function Sponsors() {
     const { t, i18n } = useTranslation();
@@ -101,6 +102,12 @@ function Sponsors() {
                         <div className="w-full lg:w-[20vw] mx-4 lg:mx-8 h-full flex flex-row items-center justify-center py-10 px-16 bg-white border-4 border-[#eddb55]">
 
                             <Image className=" w-auto h-20 scale-125" alt="degate" src={money}></Image>
+                        </div>
+                    </Link>
+                    <Link href={'https://www.criptovaluta.it/'} target="_blank" className="p-0 w-full h-fit">
+                        <div className="w-full lg:w-[20vw] mx-4 lg:mx-8 h-full flex flex-row items-center justify-center py-10 px-16 bg-white border-4 border-[#eddb55]">
+
+                            <Image className=" w-auto h-20" alt="degate" src={vault}></Image>
                         </div>
                     </Link>
                     
@@ -223,6 +230,20 @@ function Sponsors() {
                                 <div className="w-full h-full flex flex-row items-center justify-center py-10 px-16  bg-white border-4 border-[#eddb55]">
                                 <div className="w-28 h-20 scale-[1.5] bg-contain bg-center bg-no-repeat" style={{
                                         backgroundImage: `url('${money.src}')`
+                                    }}></div>
+                                </div>
+                            </Link>
+
+                        </div>
+                    </div>
+                </div>
+                <div className="w-full h-fit flex flex-row items-center justify-center">
+                    <div className="w-10/12 flex flex-col items-center justify-center">
+                        <div className="w-full flex-grow flex flex-col items-center justify-center" >
+                            <Link href={'https://www.criptovaluta.it/'} target="_blank" className="w-full h-full">
+                                <div className="w-full h-full flex flex-row items-center justify-center py-10 px-16  bg-white border-4 border-[#eddb55]">
+                                <div className="w-28 h-20 scale-[2.5] bg-contain bg-center bg-no-repeat" style={{
+                                        backgroundImage: `url('${vault.src}')`
                                     }}></div>
                                 </div>
                             </Link>
