886cf1c5b0f6803e779b84822bfe147502202661
MedNewton <med.newton.2013@gmail.com>
2024-04-02 15:04:57 -0700

    .

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,16 +5,19 @@ import Image from "next/image";
 
 import { FaXTwitter, FaLinkedin  } from "react-icons/fa6";
 import { FaTelegramPlane, FaInstagram  } from "react-icons/fa";
-import footerClouds from '@assets/footerClouds.svg'
+import footerClouds from '@assets/footerGR.svg'
 import { useTranslation } from "react-i18next";
 
-
+import pescatroie from '@assets/pescatroie 2.svg'
+import cloudBase from '@assets/cloud base2.svg'
+import totano from '@assets/totano funk 2.svg'
+import nonnetto from '@assets/nonnetto 3.svg'
 
 function Footer(){
     const { t, i18n } = useTranslation();
     return(
         <footer className="w-full h-fit pt-32 lg:pt-60 flex flex-col items-center justify-start">
-            <Image alt="footer image clouds napulEth" src={footerClouds} className="w-full h-auto scale-[1.8] lg:scale-100"></Image>
+            <Image alt="footer image clouds napulEth" src={footerClouds} className="w-full h-auto scale-[1.3] lg:scale-90 lg:-mt-40 -mb-10 lg:-mb-[8%]"></Image>
             <div className="w-full h-fit flex flex-col-reverse lg:flex-col-reverse items-center justify-between px-4 lg:px-8 py-8 lg:pt-8 lg:pb-4 bg-[#518DCA] relative z-50 mt-3 lg:-mt-5">
                 <p className="text-base SemiBold text-white mt-0 lg:mt-0 text-center lg:text-left">
                     {t('copy')}
